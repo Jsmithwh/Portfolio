4bb8859c7cb85d2441b67eac522f523dfa01242e
jsmithwh <46731513+Jsmithwh@users.noreply.github.com>
2019-06-24 09:31:13 -0500

    updated portfolio assignments

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,16 @@
 $('.navbar-burger').click(function() {
     $('#navbarBasicExample, .navbar-burger').toggleClass('is-active');
   });
+
+  //makes headers sticky
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
